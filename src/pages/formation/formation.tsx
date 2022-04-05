06bccca4bed409e7d6b124699409a008d8b44269
mld-njupt <77198035+mld-njupt@users.noreply.github.com>
2022-04-05 23:46:49 +0800

    complete separate model

--- src/pages/formation/formation.tsx
+++ src/pages/formation/formation.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Row, Col, Card, Switch } from "antd";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
@@ -6,39 +6,45 @@ import Model from "./Model";
 import "./formation.scss";
 export const FormationScreen = () => {
   const [switchsValue, setSwitchsValue] = useState({
-    "01": true,
-    "02": true,
-    "03": true,
-    "04": true,
-    "05": true,
-    "06": true,
-    "07": true,
-    "08": true,
-    "09": true,
-    "10": true,
-    "11": true,
-    "12": true,
-    "13": true,
-    "14": true,
-    "15": true,
-    "16": true,
-    "17": true,
-    "18": true,
-    "19": true,
-    "20": true,
-    "21": true,
-    "22": true,
-    "23": true,
-    "24": true,
-    "25": true,
-    "26": true,
-    "27": true,
+    0: true,
+    1: true,
+    2: true,
+    3: true,
+    4: true,
+    5: true,
+    6: true,
+    7: true,
+    8: true,
+    9: true,
+    10: true,
+    11: true,
+    12: true,
+    13: true,
+    14: true,
+    15: true,
+    16: true,
+    17: true,
+    18: true,
+    19: true,
+    20: true,
+    21: true,
+    22: true,
+    23: true,
+    24: true,
+    25: true,
+    26: true,
+    27: true,
   });
-  const handleSwitch = (no: string, ...args: any[]) => {
-    return setSwitchsValue((prev) => {
-      return { ...prev, [no]: args };
-    });
+  const handleSwitch = (no: string) => {
+    return (...args: any[]) => {
+      setSwitchsValue((prev) => {
+        return { ...prev, [no]: args[0] };
+      });
+    };
   };
+  // useEffect(() => {
+  //   console.log(switchsValue);
+  // }, [switchsValue]);
   const url =
     process.env.NODE_ENV === "development"
       ? "/api/react/Model.gltf"
@@ -62,11 +68,15 @@ export const FormationScreen = () => {
                     fontWeight: 800,
                   }}
                 >
-                  01：
+                  {value[0]}：
                 </span>
               </Col>
               <Col span={4}>
-                <Switch size="small"></Switch>
+                <Switch
+                  checked={value[1]}
+                  onChange={handleSwitch(value[0])}
+                  size="small"
+                ></Switch>
               </Col>
             </Row>
           );
@@ -84,7 +94,7 @@ export const FormationScreen = () => {
         <ambientLight intensity={0.1} />
         <directionalLight intensity={0.4} />
         <Suspense fallback={null}>
-          <Model position={[1.5, 9, -5]} url={url} />
+          <Model position={[1.5, 9, -5]} url={url} switchValue={switchsValue} />
         </Suspense>
         <OrbitControls />
       </Canvas>
